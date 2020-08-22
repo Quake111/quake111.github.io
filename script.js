c2fb925a0e03a83bcdb8a24c505ecc5472249007
Quake111 <70056040+Quake111@users.noreply.github.com>
2020-08-22 16:34:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function yonlendir() {
+    window.location = "https://yesilgazete.org/blog/2020/07/14/kopekbaliklari-hakkinda-bilmediginiz-10-carpici-gercek/" 
+}
+function yonlendir2() {
+    window.location = "https://yesilgazete.org/blog/2020/07/20/bartinda-endemik-bitki-turu-kesfedildi/"
+}
+function yonlendir3() {
+    window.location = "https://www.dunya.com/surdurulebilir-dunya/doganin-yok-olusunu-tersine-cevirecek-5-oneri-haberi-336783"
+}
+function yonlendir4() {
+    window.location = "https://gaiadergi.com/insan-yasaminda-agacin-adi-bambu-agaci/"
+}
+function yonlendir5() {
+    window.location = "https://evrimagaci.org/cekirge-istilasi-cekirgeler-neden-suruler-halinde-hareket-ediyor-8331"
+}
+function yonlendir6() {
+    window.location = "https://www.haberler.com/patara-plaji-nda-5-binden-fazla-yavru-kaplumbaga-13489228-haberi/"
+}
+function mesaj() {
+    alert("Yapım aşamasında!")
+}
+
+function yonlendir7() {
+    window.location = "https://dogasevgisi.wordpress.com/dogayi-nasil-korumaliyiz-2/"
+}
